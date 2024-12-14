7d45c18407b44c68207a3be9e1aa5308dd118afd
Georges Duplessy <gduplessy@gmail.com>
2024-12-13 23:21:18 -0500

    Initial refactoring + database

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  # Returns a title for every page
-  def title
-    base_title = 'Podil'
-    if @title.nil?
-      base_title
-    else
-      "#{@title} - #{base_title}"
-    end
-  end
 end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Podil::Application.config.secret_token = '7efb710799df395c3bae3e939c559af56aa9236f50621203fd672154d915d58aca365159a55a3d5e545b01ce644a384b57c0265a0348a5147c17c13b83ae7494'
